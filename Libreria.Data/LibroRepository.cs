a4d154294d0ab48161fe8ab21ecd0a206c240b2b
Andrea <andrea.15gros@gmail.com>
2025-09-04 18:54:19 +0200

    Aggiunto libri repository

---
+++ Libreria.Data/LibroRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libreria.Core.Models;
+
+namespace Libreria.Data
+{
+    public class LibroRepository
+    {
+        private readonly Database _db;
+
+        public LibroRepository(string connStr)
+        {
+            _db = new Database(connStr);
+        }
+
+        public List<Libro> GetNotAvaiables()
+        {
+            List < Libro > libri = new List<Libro>();
+            string query = "SELECT DISTINCT * FROM Libri l JOIN prestiti p ON l.Id = p.IdLibro;";
+            using var reader = _db.ExecuteReader(query);
+            while (reader.Read())
+            {
+                libri.Add(new Libro
+                {
+                    Id = reader.GetInt32(0),
+                    Titolo = reader.GetString(1),
+                });
+            }
+            return libri;
+        }
+    }
+}
--- Libreria.Web/Controllers/PrenotazioneController.cs
+++ Libreria.Web/Controllers/PrenotazioneController.cs
@@ -1,17 +1,20 @@
 ﻿using Libreria.Core.Models;
 using Libreria.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Libreria.Web.Controllers
 {
     public class PrenotazioneController : Controller
     {
         private readonly PrenotazioneRepository _repoPrenotazioni;
+        private readonly LibroRepository _repoLibri;
 
         public PrenotazioneController(IConfiguration configuration)
         {
             string connStr = configuration.GetConnectionString("DefaultConnection");
             _repoPrenotazioni = new PrenotazioneRepository(connStr);
+            _repoLibri = new LibroRepository(connStr);
         }
         public IActionResult Index()
         {
@@ -26,6 +29,7 @@ namespace Libreria.Web.Controllers
 
         public IActionResult Reservation()
         {
+            ViewBag.SelectLibri = new SelectList(_repoLibri.GetNotAvaiables(), "Id", "Titolo");
             return View();
         }
     }
